af3cca37da8077110df7e23337f89b0cd2a99426
Erick <erick256134@gmail.com>
2019-07-28 08:12:49 -0500

    He agregado un .gitignore

---
+++ Login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Inicio</title>
+	<link rel="stylesheet" href="css/fontawesome.min.css">
+	<link rel="stylesheet" href="css/estiloLogin.css">
+</head>
+<body>
+
+<div class="container">
+	<img src="Login/itesiLogin.png">
+	<form method="post" action="Inicio.php" autocomplete="off">
+		<div class="form-input">
+			<input type="text" name="usuario" placeholder="Usuario">	
+		</div>
+
+		<div class="form-input">
+			<input type="password" name="contra" placeholder="Contraseña">
+		</div>
+
+		<input type="submit" type="submit" value="Entrar" class="btn-login">
+	</form>
+</div>
+
+</body>
+</html>
