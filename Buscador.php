a29f72c091a6277fb6cb07944f9edb3e566592d0
Erick <erick256134@gmail.com>
2019-07-27 22:51:44 -0500

    Se agregó un h1 como prueba

--- Buscador.php
+++ Buscador.php
@@ -9,6 +9,7 @@
 </head>
 
 <body>		
+	<h1>Prueba del uso de Git</h1>
 	<nav class="navbar navbar-expand-lg navbar-light bg-success">
 	  
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
---
+++ Prestamos.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mis préstamos</title>
+	<style>
+		#contenedor{
+			width: 200px;
+			height: 200px;
+			background: green;
+			position: relative;
+		}
+
+		#caja{
+			width: 50px;
+			height: 50px;
+			background: red;
+			position: absolute;
+		}
+	</style>
+</head>
+<body>
+
+<header id="demo" style="background-color: red">
+	<a id="link" href="https://www.facebook.com">Daily</a>
+	<a href="">News</a>
+	<a href="">Forums</a>
+	<a href="">Activity</a>
+	<a id="tiempo" href=""></a>
+</header>
+
+<div id="demo2" style="background-color: blue">
+	<p>texto</p>
+	<p>más texto</p>
+	<p id="remove">elemento que será removido</p>
+	<p id="replace">elemento a ser reemplazado</p>
+</div>
+
+<div id="contenedor">
+	<div id="caja">Click me</div>
+</div>
+
+<div>
+	<button></button>
+	<img src="" alt="">
+	<button></button>
+</div>
+
+
+<script src="javascriptTutorial.js"></script>
+<script src="javascriptTutorial2.js"></script>
+</body>
+</html>
